94cd1aea29891aa24268c706f807f9b70a828b91
Vidhi Jain <vidhijain507@gmail.com>
2025-02-19 16:48:12 +0530

    [Vidhi_Jain]Add. Code to Validate an email field using JSON Schema.

---
+++ src/main/java/com/day2jsonhandling/handsonpracticeproblems/validateemail/ValidateUsingJSONSchema.java
@@ -0,0 +1,34 @@
+package com.day2jsonhandling.handsonpracticeproblems.validateemail;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.fge.jsonschema.core.report.ProcessingReport;
+import com.github.fge.jsonschema.main.JsonSchema;
+import com.github.fge.jsonschema.main.JsonSchemaFactory;
+import java.io.File;
+
+public class ValidateUsingJSONSchema {
+    public static boolean isValidJson(String jsonFilePath, String schemaFilePath) {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode jsonData = objectMapper.readTree(new File(jsonFilePath)); // Read JSON array
+            JsonNode jsonSchema = objectMapper.readTree(new File(schemaFilePath)); // Read schema
+
+            JsonSchemaFactory factory = JsonSchemaFactory.byDefault();
+            JsonSchema schema = factory.getJsonSchema(jsonSchema);
+
+            ProcessingReport report = schema.validate(jsonData);
+            System.out.println(report); // Print detailed validation report
+
+            return report.isSuccess();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        boolean isValid = isValidJson("src/main/resources/Employee.json", "src/main/resources/email-schema.json");
+        System.out.println("Is JSON valid? " + isValid);
+    }
+}
