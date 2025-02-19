1063a1511f20f22cfed02d15f7337b2f77a188da
Vidhi Jain <vidhijain507@gmail.com>
2025-02-19 16:31:59 +0530

    [Vidhi_Jain]Add. Code to Convert a list of Java objects into a JSON array.

---
+++ src/main/java/com/day2jsonhandling/handsonpracticeproblems/listtoarray/JavaListToJSONArray.java
@@ -0,0 +1,28 @@
+/*2️⃣ Convert a list of Java objects into a JSON array.*/
+package com.day2jsonhandling.handsonpracticeproblems.listtoarray;
+
+import com.day2jsonhandling.practiceproblems.listtojsonarray.Person;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class JavaListToJSONArray {
+    public static String convertListToJSON(List<com.day2jsonhandling.practiceproblems.listtojsonarray.Person> personList){
+        try{
+            ObjectMapper objectMapper = new ObjectMapper();
+            return objectMapper.writeValueAsString(personList);
+        }catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+    public static void main(String[] args) {
+        List<com.day2jsonhandling.practiceproblems.listtojsonarray.Person> people = Arrays.asList(
+                new com.day2jsonhandling.practiceproblems.listtojsonarray.Person("Vedant", 20),
+                new Person("Vedvyas", 52)
+        );
+        String jsonArray = convertListToJSON(people);
+        System.out.println(jsonArray);
+    }
+}
